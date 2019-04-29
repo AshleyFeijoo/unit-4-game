017c22b60f221c239e5482d07311e1d922cdc598
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-04-29 17:07:29 -0500

    Fixed more bugs with the sound and timing things out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,123 +1,116 @@
-$(document).ready(function () {
-//=============+++++++++++++++++++================//
-    //Global Variables// and sets initial text
-//=============+++++++++++++++++++================//
-    var wins = 0;
-    var losses = 0;
-    var totalScore = 0;
-    var selectedValue = "";
+$(document).ready(function() {
+  //=============+++++++++++++++++++================//
+  //Global Variables// and sets initial text
+  //=============+++++++++++++++++++================//
+  var wins = 0;
+  var losses = 0;
+  var totalScore = 0;
+  var selectedValue = "";
 
-    $("#wins").text(wins);
-    $("#losses").text(losses);
-    $("#total-score").text(totalScore);
+  $("#wins").text(wins);
+  $("#losses").text(losses);
+  $("#total-score").text(totalScore);
 
+  //=============+++++++++++++++++++================//
+  //Selects main random number score and displays it//
+  //=============+++++++++++++++++++================//
+  function randomCrystalNum(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
-//=============+++++++++++++++++++================//
-    //Selects main random number score and displays it//
-//=============+++++++++++++++++++================//
-    function randomCrystalNum(min, max) {
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+  var randomCompChoice = randomCrystalNum(19, 120);
+  $("#compChoiceNum").text(randomCompChoice);
 
-    var randomCompChoice = randomCrystalNum(19, 120);
-    $("#compChoiceNum").text(randomCompChoice);
+  //=============+++++++++++++++++++================//
+  //Selects a random value for each crystal//
+  //=============+++++++++++++++++++================//
+  var ranValue = [];
+  var currentValue;
 
-//=============+++++++++++++++++++================//
-    //Selects a random value for each crystal//
-//=============+++++++++++++++++++================//
-    var ranValue = [];
-    var currentValue;
-    function generateNum(){
+  function generateNum() {
     while (ranValue.length <= 4) {
       currentValue = Math.floor(Math.random() * 12) + 1;
       if (ranValue.indexOf(currentValue) < 0) {
-     ranValue.push(currentValue);
+        ranValue.push(currentValue);
       }
     }
-};
-generateNum(12);
+  }
+  generateNum(12);
 
-console.log("the randomcompnum is: " + randomCompChoice);
-//=============+++++++++++++++++++================//
-    //Reset Function
-//=============+++++++++++++++++++================//
-function resetGame(){
+  console.log("the randomcompnum is: " + randomCompChoice);
+  //=============+++++++++++++++++++================//
+  //Reset Function
+  //=============+++++++++++++++++++================//
+  function resetGame() {
+    $(".buttonS").prop("disabled", false);
     ranValue = [];
     generateNum(12);
     totalScore = 0;
     randomCompChoice = randomCrystalNum(19, 120);
     $("#compChoiceNum").text(randomCompChoice);
     $("#total-score").text(totalScore);
-};
+  }
 
-//=============+++++++++++++++++++================//
-    //functions for win and lose 
-//=============+++++++++++++++++++================//
-    function youWon(){
-        wins++;
-        alert("You won!");
-        $("#pop-up").show();
-        $(".container").hide();
-        $("#wins").text(wins);
-        resetGame();
-        
-    };
+  //=============+++++++++++++++++++================//
+  //functions for win and lose
+  //=============+++++++++++++++++++================//
+  function youWon() {
+    wins++;
+    alert("You won!");
+    $("#wins").text(wins);
+    resetGame();
+  }
 
-    function youLost(){
-        losses++;
-        $(".container").hide();
-        // alert("You Lose! Your total score was: " + totalScore);
-        $("#losses").html(losses);
-        
-        resetGame();
-    };
-    // =======================++++++++++++++++++
+  function youLost() {
+    losses++;
+    alert("You Lose! Your total score was: " + totalScore);
+    $("#losses").html(losses);
+    resetGame();
+  }
 
- 
-    
+  //=============+++++++++++++++++++================//
+  //Begins game with click
+  //=============+++++++++++++++++++================//
 
-//=============+++++++++++++++++++================//
-    //Begins game with click 
-//=============+++++++++++++++++++================//
-    //Sound Variables//
-//=============+++++++++++++++++++================//
-var sound = new Audio("click.wav");
+  var selectedValue = "";
+  $(".buttonS").on("click", function() {
+    $("#button-1").val(ranValue[0]);
+    $("#button-2").val(ranValue[1]);
+    $("#button-3").val(ranValue[2]);
+    $("#button-4").val(ranValue[3]);
+    selectedValue = parseInt($(this).val(), 10);
+    totalScore = totalScore + selectedValue;
+    $("#total-score").text(totalScore);
 
-var selectedValue = "";
+    //=============+++++++++++++++++++================//
+    //checks if conditions are true and disables the buttons temporarily
+    //=============+++++++++++++++++++================//
+    if (totalScore > randomCompChoice) {
+      $(".buttonS").prop("disabled", true);
+      setTimeout(youLost, 750);
+    } else if (totalScore === randomCompChoice) {
+      $(".buttonS").prop("disabled", true);
+      setTimeout(youWon, 750);
+    }
+  });
+  //=============+++++++++AUDIO++++++++++================//
+  $("#button-1").on("click", function() {
+    $("audio#pop")[0].play();
+    $("audio#pop")[0].currentTime = 0;
+  });
 
+  $("#button-2").on("click", function() {
+    $("audio#pop2")[0].play();
+    $("audio#pop2")[0].currentTime = 0;
+  });
 
-    $(".buttonS").on("click", function(){
-        $("#button-1").val(ranValue[0]);
-        $("#button-2").val(ranValue[1]);
-        $("#button-3").val(ranValue[2]);
-        $("#button-4").val(ranValue[3]);
-        selectedValue = parseInt($(this).val(), 10);
-//================AUDIO====================================
-        audio = new Audio("click.wav");
-        sound.preload = 'auto';
-        sound.volume = 0.2;
-        // sound.load();
-        function playSound() {
-        var click=sound.cloneNode();
-        click.play();  
-        }
-        playSound();
+  $("#button-3").on("click", function() {
+    $("audio#pop3")[0].play();
+    $("audio#pop3")[0].currentTime = 0;
+  });
 
-        totalScore = totalScore + selectedValue;
-        $("#total-score").text(totalScore);
-        if(totalScore > randomCompChoice){
-            youLost();
-            resetGame();
-
-        
-        } else if(totalScore === randomCompChoice){
-            youWon();
-
-        }
-
-      });
-    
-
+  $("#button-4").on("click", function() {
+    $("audio#pop4")[0].play();
+    $("audio#pop4")[0].currentTime = 0;
+  });
 }); //function document ready end *****************
-
-
